78a02d3b1530f1c8156a47b88f7967fcf67216ab
Prianti Banik <59369461+prianti29@users.noreply.github.com>
2026-01-19 17:52:02 +0600

    feat: Add Playwright test for locked-out user login and simplify Playwright configuration with a base URL.

---
+++ tests/login.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require("@playwright/test");
+
+test("Login with locked_out_user Credentials", async ({ page }) => {
+  await page.goto("/");
+  //get title
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Swag Labs");
+  //css , xpath
+  const email = page.getByRole('textbox', { name: 'Username' });
+  const password = page.getByRole('textbox', { name: 'Password' });
+
+  await email.fill("locked_out_user");
+  await password.fill("secret_sauce");
+
+  await page.getByTestId('login-button').click();
+
+  await expect(page.getByRole('heading', { name: 'Epic sadface: Sorry, this user has been locked out.' })).toBeVisible();
+});
+
+
